ea23710d4dee665cc747bb63cde395a44aec2637
thaivipprook <thaipro9t@gmail.com>
2025-03-02 21:39:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "",
+    introTitle: 'linh à',
+    introDesc: ``,
+    btnIntro: '^^HiHi^^',
+    title: 'mày có yêu tao ko?',
+    desc: '',
+    btnYes: 'có',
+    btnNo: 'không',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao bạn lại yêu tao <3',
+    btnReply: 'Gửi cho Thái <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'tao biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: '',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100091710934984/' //link mess của các bạn. VD: https://www.facebook.com/profile.php?id=100091710934984
+}
